302219c27c348092f01633ce01a20d23bbba3a56
atif <atifahamad913@gmail.com>
2025-12-25 19:47:49 +0530

    updated bot.py

--- bot.py
+++ bot.py
@@ -8,8 +8,6 @@ from pyrogram import Client
 from pyrogram.enums import ParseMode
 import sys
 from datetime import datetime
-import pyrogram.utils
-pyrogram.utils.MIN_CHANNEL_ID = -1009999999999
 
 from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, CHANNEL_ID, PORT
 
